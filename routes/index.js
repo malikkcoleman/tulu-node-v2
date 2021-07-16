f6c9ba24226ed43fe5aaeb67e68e582922c7b41e
ltapsy <drolxiv@gmail.com>
2021-07-16 00:05:31 -0600

    fixed conflict

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const pgroutr = express.Router();
-<<<<<<< Updated upstream
 const ROLE = require('../roles');
 const {ensureAuthenticated, authRole} = require('../config/auth');
-=======
-const { ensureAuthenticated } = require("../config/auth");
->>>>>>> Stashed changes
 
 const Vehicle = require("../models/vehicleSchema");
 const User = require("../models/userSchema");
 
-<<<<<<< Updated upstream
 pgroutr.get('/',(req,res)=>
     res.render('index',{
     user:req.user
@@ -54,101 +49,12 @@ pgroutr.get('/carview',(req,res)=>{
         })
     }).catch((err)=>{
         res.status(500).send(error);
-=======
-pgroutr.get("/", (req, res) =>
-  res.render("index", {
-    role: "guest",
-  })
-);
-
-pgroutr.get("/dashboard", ensureAuthenticated, (req, res) =>
-  res.render("dashboard", {
-    name: req.user.name,
-    name: req.user.email,
-  })
-);
-
-pgroutr.get("/tulu", ensureAuthenticated, (req, res) =>
-  res.render("tulu", {
-    fName: req.user.toObject().fName,
-    lName: req.user.toObject().lName,
-    linkedin: req.user.toObject().linkedin,
-    instagram: req.user.toObject().instagram,
-    facebook: req.user.toObject().facebook,
-    bio: req.user.toObject().bio,
-    experience: req.user.toObject().experience,
-    specialty: req.user.toObject().specialty,
-    favoriteCar: req.user.toObject().favoriteCar,
-    currentCar: req.user.toObject().currentCar,
-    phoneNumber: req.user.toObject().phoneNumber,
-    email: req.user.toObject().email,
-    role: req.user.toObject().role,
-  })
-);
-
-pgroutr.get("/profile", ensureAuthenticated, (req, res) =>
-  res.render("profile", {
-    fName: req.user.toObject().fName,
-    lName: req.user.toObject().lName,
-    linkedin: req.user.toObject().linkedin,
-    instagram: req.user.toObject().instagram,
-    facebook: req.user.toObject().facebook,
-    bio: req.user.toObject().bio,
-    experience: req.user.toObject().experience,
-    specialty: req.user.toObject().specialty,
-    favoriteCar: req.user.toObject().favoriteCar,
-    currentCar: req.user.toObject().currentCar,
-    phoneNumber: req.user.toObject().phoneNumber,
-    email: req.user.toObject().email,
-    role: req.user.toObject().role,
-  })
-);
-
-pgroutr.get("/dashboardsysadmin", ensureAuthenticated, (req, res) =>
-  res.render("dashboardsysadmin", {
-    fName: req.user.toObject().fName,
-    lName: req.user.toObject().lName,
-    linkedin: req.user.toObject().linkedin,
-    instagram: req.user.toObject().instagram,
-    facebook: req.user.toObject().facebook,
-    bio: req.user.toObject().bio,
-    experience: req.user.toObject().experience,
-    specialty: req.user.toObject().specialty,
-    favoriteCar: req.user.toObject().favoriteCar,
-    currentCar: req.user.toObject().currentCar,
-    phoneNumber: req.user.toObject().phoneNumber,
-    email: req.user.toObject().email,
-    role: req.user.toObject().role,
-  })
-);
-
-pgroutr.get("/shop", (req, res) => {
-  Vehicle.find({})
-    .then((vehicles) => {
-      res.render("shop", {
-        vehicles: vehicles,
-        role: "guest",
-      });
-    })
-    .catch((err) => {
-      res.status(500).send(error);
-    });
-});
-pgroutr.get("/carview", (req, res) => {
-  Vehicle.find({})
-    .then((vehicles) => {
-      res.render("carview", {
-        vehicles: vehicles,
-        role: "guest",
-      });
->>>>>>> Stashed changes
     })
     .catch((err) => {
       res.status(500).send(error);
     });
 });
 
-<<<<<<< Updated upstream
 
 pgroutr.get('/tululist',(req,res)=>{
     User.find({role:'tulu'}).then((tulu)=>{
@@ -158,32 +64,15 @@ pgroutr.get('/tululist',(req,res)=>{
         })
     }).catch((err)=>{
         res.status(500).send(error);
-=======
-pgroutr.get("/tululist", (req, res) => {
-  User.find({ role: "tulu" })
-    .then((tulu) => {
-      res.render("tululist", {
-        tulu: tulu,
-        role: "guest",
-      });
->>>>>>> Stashed changes
     })
     .catch((err) => {
       res.status(500).send(error);
     });
 });
 
-<<<<<<< Updated upstream
 pgroutr.get('/:page', function(req, res){
     res.render(req.params.page,{
         user:req.user.toObject()
 })});
-=======
-pgroutr.get("/:page", function (req, res) {
-  res.render(req.params.page, {
-    role: "guest",
-  });
-});
->>>>>>> Stashed changes
 
 module.exports = pgroutr;
