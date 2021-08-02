b879f78e9e07a47aa265c443ea6adcc9653619d2
Malik Coleman <malikcoleman@live.ca>
2021-08-02 16:34:06 -0500

    starting leads table

---
+++ models/buynowschema.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const buynowschema = new mongoose.Schema({
+  targetId: {
+    type: String,
+    required: true,
+  },
+  vin: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  province: {
+    type: String,
+    required: true,
+  },
+  postal: {
+    type: String,
+    required: true,
+  },
+});
+
+const buynowschemas = mongoose.model("buynow", buynowschema);
+
+module.exports = buynowschemas;
