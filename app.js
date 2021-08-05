9f5b6018712ab3319b07b94df9fca295154a171c
ltapsy <drolxiv@gmail.com>
2021-08-05 15:58:13 -0600

    view tulu from tuluList + populated the page with tulu Info

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ app.use("/vehicles", require("./routes/vehicles"));
 app.use("/dealerships", require("./routes/dealerships"));
 app.use("/sysadminvehicles", require("./routes/sysAdminVehicles"));
 app.use("/vin", require("./routes/vinDecoder"));
+app.use("/tulu", require("./routes/tulu"));
 // ---------------------------------
 
 app.use(express.static(__dirname + "/public"));
